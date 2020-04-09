c5d8d9bfddb0caaa5166f42a990eba144dccdf74
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-09 02:49:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,7 +91,7 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-alans_new_info
+alans_new_info = "GUI"
 
 	programmer_hash = 
  		{
@@ -108,7 +108,7 @@ alans_new_info
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for] = 
+programmer_hash[:alan_kay][:known_for] = alans_new_info
 programmer_hash
 end
 
